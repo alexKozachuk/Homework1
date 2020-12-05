6a92a4c3b495b278355258d1d18443571431ff11
alexKozachuk <grayid@icloud.com>
2020-12-05 12:12:25 +0200

    Add some limitation for input

--- Homework1/MathService.swift
+++ Homework1/MathService.swift
@@ -13,7 +13,7 @@ class MathService {
     private init() {}
     
     func factorialIterate(n: Int) -> Int {
-        if n > 20 { return 0 }
+        if n > 20 || n < 0 { return 0 }
         if n == 0 { return  1 }
         
         var temp = 1
@@ -25,6 +25,7 @@ class MathService {
     }
     
     func factorialRecurtion(n: Int) -> Int {
+        if n < 0 { return 0 }
         if n > 20 { return 0 }
         if n >= 1 {
             return n * factorialRecurtion(n: n - 1)
@@ -34,6 +35,7 @@ class MathService {
     }
     
     func fibonachi(n: Int) -> [Int] {
+        if n <= 0 { return [] }
         if n == 1 { return [1] }
         var arr: [Int] = [1]
         var a = 0
@@ -51,6 +53,7 @@ class MathService {
     }
     
     func pi(n: Int) -> Double{
+        if n <= 0 { return 0 }
         var temp: Double = 0
         for k in 0..<n {
             let a: Double =  1 / pow(16.0, Double(k))
